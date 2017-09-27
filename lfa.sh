70bcdec88e2379b567ae17df96356744882aa2f1
trulio2 <tulio-santos@hotmail.com>
2017-09-27 02:57:01 -0300

    .

--- lfa.sh
+++ lfa.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 checkfinal()
 {
- for (( k = 0; k < ${#estadosfinais[@]}; k++ )); do
-  if [[ $atual == ${estadosfinais[$k]} ]]; then
+ for (( k = 0; k < ${#finals[@]}; k++ )); do
+  if [[ $atual == ${finals[$k]} ]]; then
    echo "Sim"
    break
   fi
  done
- if [[ $k -eq ${#estadosfinais[@]} ]]; then
+ if [[ $k -eq ${#finals[@]} ]]; then
    echo "Não"
  fi
 }
@@ -16,13 +16,13 @@ mover()
 {
  for (( n = 0; n < ${#grafo[@]}; n+=3  )); do
   if [[ $atual -eq ${grafo[$n]} && ${teste[$j]} == ${grafo[$n+1]} ]]; then
+echo -n "$atual -> "
        atual=${grafo[$n+2]}
+echo $atual
        break
   fi
  done
 }
-
-
 
 if [[ $# == 0  ]]; then
 	echo "Usar: $0 [AFNλ]"
@@ -48,9 +48,8 @@ for arq in $(cat $1); do
  fi
 
 done
-
-estadosiniciais=$(echo "$estado" | sed "s:\[::g" | sed "s:\ ::g" | sed "s:,: :g" | awk -F"]" '{print $1}')
-estadosfinais=$(echo "$estado" | sed "s:\[::g" | sed "s:\ ::g" | sed "s:,: :g" | awk -F"]" '{print $2}')
+initials=$(echo "$estado" | sed "s:\[::g" | sed "s:\ ::g" | sed "s:,: :g" | awk -F"]" '{print $1}')
+finals=$(echo "$estado" | sed "s:\[::g" | sed "s:\ ::g" | sed "s:,: :g" | awk -F"]" '{print $2}')
 
 estados=$(echo "$estadosiniciais" | sed "s:\[::g" | sed "s:\ ::g" | sed "s:,: :g" | awk -F"]" '{print $1}')
 
@@ -58,25 +57,27 @@ transicoes=$(echo "$estadosiniciais" | sed "s:\[::g" | sed "s:\ ::g" | sed "s:,:
 
 estados=(${estados})
 
-for (( i=0; i<${#estados[@]}; i++ )); do
+for (( i = 0; i < ${#estados[@]}; i++ )); do
   grafo=$(echo $grafo | sed "s:${estados[$i]}:$i:g")
-  estadosfinais=$(echo $estadosfinais | sed "s:${estados[$i]}:$i:g")
+  initials=$(echo $initials | sed "s:${estados[$i]}:$i:g")
+  finals=$(echo $finals | sed "s:${estados[$i]}:$i:g")
   estados[$i]=$i
 done
 
-estadosfinais=(${estadosfinais})
-estadosiniciais=(${estadosiniciais})
+finals=(${finals})
+initials=(${initials})
 transicoes=(${transicoes})
 grafo=(${grafo})
-remover=$(echo "${#grafo[@]} - ${#estadosfinais[@]}" | bc -l)
+remover=$(echo "${#grafo[@]} - ${#finals[@]} - ${#initials[@]}" | bc -l)
 
 for (( i = ${#grafo[@]}; i >= $remover; i-- )); do
     grafo[$i]=''
 done
 
+
 while true; do
  read teste
- atual=${estadosiniciais[0]}
+ atual=${initials[0]}
  teste=$(echo ${teste} | grep -o .)
  teste=(${teste})
 
